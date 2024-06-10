f24b3c59c32883e0bfb0499a81cb5e1d901374f6
kalidas <avengers@gmail.com>
2024-06-10 13:50:04 -0400

    change triangle to circle

--- m2p.java
+++ m2p.java
@@ -10,5 +10,5 @@ public static void calculateArea(double length, double width) {
   double area = length * width;
   
   // Print a message with the calculated area
-  System.out.println("The area of the triangle is: " + area);
+  System.out.println("The area of the circle is: " + area);
 }
