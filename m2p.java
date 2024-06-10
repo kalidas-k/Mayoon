b795cf9f98ccde50813ded1d16f960f6cc5c8d5f
Vilgax <vilgax@kali>
2024-06-10 13:33:52 -0400

    initial commit

---
+++ m2p.java
@@ -0,0 +1,14 @@
+/**
+ * This method calculates the area of a rectangle
+ * and prints it to the console.
+ * 
+ * @param length The length of the rectangle (double)
+ * @param width The width of the rectangle (double)
+ */
+public static void calculateArea(double length, double width) {
+  // Calculate the area by multiplying length and width
+  double area = length * width;
+  
+  // Print a message with the calculated area
+  System.out.println("The area of the rectangle is: " + area);
+}
